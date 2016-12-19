b2c0202024750f4d83d1851a6ea4ef121718c36e
ionut.iacob <ionut.iacob17@gmail.com>
2016-12-19 17:38:32 +0200

    Integrated with Output module. Added placeholder JSON and post request method.

--- Integrare (Server)/index.php
+++ Integrare (Server)/index.php
@@ -1,6 +1,22 @@
 <?php
 	header('Content-Type: application/json');
 
+	//POST request function
+	function post_to($url, $data){
+		$options = array(
+			'http' => array(
+				'header'  => "Content-type: application/json",
+				'method'  => 'POST',
+				'content' => $data
+			)
+		);
+		$context  = stream_context_create($options);
+		$result = file_get_contents($url, false, $context);
+
+		return json_decode($result);
+	}
+
+	//Parse message
 	if ( isset($_REQUEST["input"]) ){
 		//Get message from client
 		$currentInput = $_REQUEST["input"];
@@ -13,6 +29,14 @@
 		$textProcessingPath = 'http://localhost:2000?input='.urlencode($currentInput);
 		$textProcessingResponse = file_get_contents($textProcessingPath);
 		$finalJson["text_processing"] = json_decode($textProcessingResponse);
+
+		//Call output
+		$outputArray = array("action" => "answer_what", "question" => "what is your favorite food?");
+		$outputInput = json_encode($outputArray);
+		$outputPath = 'http://localhost:7000';
+		$finalJson["output"] = post_to($outputPath, $outputInput);
+
+		//Final
 		echo json_encode($finalJson);
 	}
 
