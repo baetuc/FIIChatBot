41a03c3516dabae9e67e0d9c376393d4a1dce66b
ionut.iacob <ionut.iacob17@gmail.com>
2017-01-06 13:43:55 +0200

    Added documentation servers

--- Integrare (Server)/index.php
+++ Integrare (Server)/index.php
@@ -1,19 +1,27 @@
 <?php
 	header('Content-Type: application/json');
 
+	//Server definitions
+	$textProcessingURL = 'http://localhost:2000';
+	$databaseURL = 'http://localhost:4000';
+	$outputURL = 'http://localhost:7000';
+
+
 	//POST request function
-	function post_to($url, $data){
+	function post_to($url, $jsonData){
 		$options = array(
 			'http' => array(
 				'header'  => "Content-type: application/json",
 				'method'  => 'POST',
-				'content' => $data
+				'content' => $jsonData
 			)
 		);
 		$context  = stream_context_create($options);
 		$result = file_get_contents($url, false, $context);
 
-		return json_decode($result);
+		//Removing headers
+		$parsedResult = split("\r\n\r\n", $result, 2);
+		return json_decode($parsedResult[1]);
 	}
 
 	//Parse message
@@ -26,18 +34,23 @@
 		$finalJson["input"] = $currentInput;
 
 		//Call text processing
-		$textProcessingPath = 'http://localhost:2000?input='.urlencode($currentInput);
+		$textProcessingPath = $textProcessingURL.'?input='.urlencode($currentInput);
 		$textProcessingResponse = file_get_contents($textProcessingPath);
+		$overloadedTextProcessingResponse = json_encode(array("sentences" => json_decode($textProcessingResponse), "numberSentences"=>1));
 		$finalJson["text_processing"] = json_decode($textProcessingResponse);
 
-		//Call output
-		$outputArray = array("action" => "answer_what", "question" => "what is your favorite food?");
-		$outputInput = json_encode($outputArray);
-		$outputPath = 'http://localhost:7000';
-		$finalJson["output"] = post_to($outputPath, $outputInput);
+		//Call database
+		$databasePath = $databaseURL;
+		$databaseResponse = post_to($databasePath, $overloadedTextProcessingResponse);
+		$finalJson["database"] = $databaseResponse;
 
-		//Final
+		//Call output
+		$outputInput = $databaseResponse;
+		$outputPath = $outputURL;
+		$outputResponse = post_to($outputPath, $outputInput);
+		$finalJson["output"] = $outputResponse;
+
+		// Final
 		echo json_encode($finalJson);
 	}
-
 ?>
