9caa0668f372882b09cc1dc8eee95b775c4818fe
ionut.iacob <ionut.iacob17@gmail.com>
2017-01-08 13:26:35 +0200

    Updated interface with ajax call to server / enter fire

--- Interfata/js/home.js
+++ Interfata/js/home.js
@@ -1,9 +1,21 @@
-var localURL = 'localhost:1000';
+var localURL = 'http://localhost:1000';
+
+
+
+$( document ).ready(function() {
+	$("#input-left-position").on('keypress', function(e){
+		console.log("fired key");
+		if(e.which == 13) {
+			console.log("fired");
+			SendUserMessage();
+		}
+	});
+});
 
 function GetBotAnswer(userText){
 	$.ajax(
 		{
-			url: localURL+'getMessage', 
+			url: localURL+'getMessage',
 			data: userText,
 			success: function(result){
 				SendBotMessage(result);
@@ -36,7 +48,8 @@ function SendBotMessage(message){
 function SendUserMessage(){
 	var inputBox = $('#input-left-position')[0];
 	var message = inputBox.value;
-	
+
+	//Adding to frontend
 	 $('.chat').append('<li class=\"right clearfix\"><span class=\"chat-img pull-right\">\
 							<img src=\"http://placehold.it/50/55C1E7/fff&text=ME\" alt=\"User Avatar\" class=\"img-circle\" />\
 							</span>\
@@ -52,4 +65,24 @@ function SendUserMessage(){
 						 </div>\
 						</li>');
 	inputBox.value = "";
+
+	//Send input to server
+	$.ajax(
+		{
+			url: localURL,
+			type: 'get',
+			data: {
+				'input': message
+			},
+			success: function(result){
+				console.log(result);
+				SendBotMessage(result.database.response[0].AI);
+				// SendBotMessage(result);
+			},
+			error: function(result){
+				console.log(result);
+			}
+		}
+	);
+
 }
