39851df5ea39826267f8b161717cdcd92dfe1234
Leocrux <radu.romaniuc@gmail.com>
2017-01-24 20:24:24 +0200

    Modificat DB server 2 noi rute: 	1. /reset - pt reset la ontologii (End of conv) 	2. /inactivity - todo

--- DataBase/server.py
+++ DataBase/server.py
@@ -1,51 +1,24 @@
-
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-import httplib,socket, urllib
 import simplejson
 import BDHandler
+from bottle import run, post, request, response, get, route
+
+@route('/',method = 'POST')
+def process():
+    data = request.body.read()
+    data = simplejson.loads(data)
+    responseBDH = BDHandler.init(data)
+    return simplejson.dumps(responseBDH)
 
 
-class myHandler(BaseHTTPRequestHandler):
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-
-        self.wfile.write("Hello World !")
-        return
-
-    def do_POST(self):
-        self._set_headers()
-        print "POST"
-        self.data_string = self.rfile.read(int(self.headers['Content-Length']))
-        self.send_response(200)
-        self.end_headers()
-
-        data = simplejson.loads(self.data_string)
-
-        responseBDH = BDHandler.init(data)
-
-        pass
-
-        try:
-            self.wfile.write(simplejson.dumps(responseBDH))
-        except socket.error:
-            print "Failed to send data."
-        return
+@route('/reset',method = 'POST')
+def reset():
+    status = BDHandler.resetOntologii()
+    return status # True / None
 
 
-PORT_NUMBER = 4000
-try:
-    server = HTTPServer(('127.0.0.1', PORT_NUMBER), myHandler)
-    print 'Started server on port ', PORT_NUMBER
+@route('/inactivity',method = 'POST')
+def inactivity():
+    pass #TODO
 
-    server.serve_forever()
 
-except KeyboardInterrupt:
-    print '^C received, shutting down the web server'
-    server.socket.close()
+run(host='localhost', port=4000, debug=True)
